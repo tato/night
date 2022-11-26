a00ec22c43ebaf29ccbfc7a6504b756a9ab5cf33
pablo <pabtatoramos@gmail.com>
2022-11-26 17:26:50 +0100

    engine: set permissive cors for debug mode

--- engine/main.go
+++ engine/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"supok.es/night/basepath"
@@ -8,7 +11,21 @@ import (
 )
 
 func main() {
+
+	opt := parseArguments()
+
+	if !opt.debugMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	router := gin.Default()
+
+	if opt.debugMode {
+		router.Use(cors.New(cors.Config{
+			AllowAllOrigins: true,
+			AllowMethods:    []string{"GET", "POST", "PUT", "DELETE"},
+		}))
+	}
 
 	router.GET("/basepath", basepath.GetBasePath)
 	router.PUT("/basepath", basepath.PutBasePath)
@@ -17,4 +34,20 @@ func main() {
 	router.GET("/files/*path", files.GetFile)
 
 	router.Run("localhost:9001")
+}
+
+type options struct {
+	debugMode bool
+}
+
+func parseArguments() options {
+	var opt options
+
+	for _, arg := range os.Args[1:] {
+		if arg == "--debug" {
+			opt.debugMode = true
+		}
+	}
+
+	return opt
 }
